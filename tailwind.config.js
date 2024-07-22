bf86b468d25bdd09f4cf86d989bbdd5da53f6bd3
lamidimorufat <lamidimorufat0@gmail.com>
2024-07-22 22:15:48 +0100

    feat: created the user interface of the website

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,25 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px',
+    },
+    extend: {
+      colors : {
+        DarkGreen: '#004343',
+        MintGreen: '#77E6B6',
+        DeepGreen: '#1A5319'
+
+      }
+    },
   },
   plugins: [],
 }
 
+
+
+
+ 
